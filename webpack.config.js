d7c77b808b4fd97a9fc9c9df355f5f7c939785d5
lgq <kuchcy@163.com>
2018-08-02 21:49:47 +0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '今天也要加油鸭'
+    })
+  ]
+}
