bde385c445c1d3237a002d534ec1556a28f93c8d
lgq <kuchcy@163.com>
2018-08-07 22:43:51 +0800

    readme

--- README.md
+++ README.md
@@ -2,6 +2,33 @@
   <img width="320" src="./logo.svg">
 </p>
 
-# blade
+## 目标
 
 旨在打造自己顺手的工具库，并且以项目驱动学习
+
+## 项目技术栈
+
+本项目基于`Electron`，`Node`，`React`，`Antd`构建，图标库选用`font-awesome`，Eslint 规范选用`airbnb`，现在项目还未引入`redux`，当需要的时候自然会引入的。
+
+## 文件结构
+
+该项目的 UI 类似后台，公用的部分放在公用的部分，比如 src 文件夹下，公用的组件（components），公用的一些资源（assets），公用的功能函数（utils），作为开发者，当你有一个好点子，不妨你自己想做的功能则放置于 views 文件夹中（需在 views 文件夹中新建一个文件夹，你可以在你的文件夹下面随意操作）
+
+### src 文件夹
+
+`index` 则是整个应用的入口
+`App.js` 负责项目的布局
+`routes/index.js` 负责项目的路由
+`assets` 公共的资源
+`components` 公共的组件
+`utils` 公共的功能函数
+`views` 单个页面
+
+eslint，babel 配置文件都在 package.json 文件中
+
+```js
+```
+
+## 工作流
+
+执行操作请新开分支，分支命名规则,如 `feat/picToResize@lgq`
--- src/App.js
+++ src/App.js
@@ -1,6 +1,53 @@
 import React from 'react'
-import Layout from './Layout'
-export default function App() {
-  return <Layout />
-  // return <FileSelect />
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { Layout, Menu, Icon } from 'antd'
+import routes from './routes'
+const { Header, Content, Footer, Sider } = Layout
+
+export default function PageLayout() {
+  return (
+    <Router>
+      <Layout>
+        <Sider
+          style={{
+            overflow: 'auto',
+            height: '100vh',
+            position: 'fixed',
+            left: 0
+          }}
+        >
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['0']}>
+            {routes.map((route, idx) => {
+              return (
+                <Menu.Item key={idx}>
+                  <Link to={route.path}>
+                    <Icon type={route.type} />
+                    <span className="nav-text">{route.name}</span>
+                  </Link>
+                </Menu.Item>
+              )
+            })}
+          </Menu>
+        </Sider>
+        <Layout style={{ marginLeft: 200 }}>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+            <div style={{ padding: 24, background: '#fff' }}>
+              {routes.map((route, index) => (
+                <Route
+                  key={index}
+                  path={route.path}
+                  exact={route.exact}
+                  component={route.main}
+                />
+              ))}
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Blade ©2018 Better Late Than Never
+          </Footer>
+        </Layout>
+      </Layout>
+    </Router>
+  )
 }
